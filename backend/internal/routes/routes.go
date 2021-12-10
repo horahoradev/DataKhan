bab1931fa2aa4ca0cecde401a0f024faedd674e5
horahoradev <horahora1567@gmail.com>
2021-12-10 11:35:03 -0800

    Benchmark working

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -8,7 +8,9 @@ import (
 	echo "github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 	"io/ioutil"
+	"net/http"
 	"net/url"
+	"strings"
 )
 
 type RouteHandler struct {
@@ -41,12 +43,28 @@ func (r *RouteHandler) handleView(c echo.Context) error {
 	}
 
 	// Increment the number of requests for path
-	r.CreateCounter(fmt.Sprintf(".%s.NumberRequests;path=%s;useragent=%s;ip=%s", uri.Hostname(), uri.Path, parsed.Useragent, parsed.IP)).Add(1)
+	r.CreateCounter(fmt.Sprintf(".%s.NumberRequests;path=%s;useragent=%s;ip=%s;hostname=%s", uri.Hostname(), uri.Path, parsed.Useragent, parsed.IP, strings.Replace(uri.Hostname(), ".", "", -1))).Add(1)
 	r.CreateCounter("."+uri.Hostname()+".NumberRequests").With("path", uri.Path).Add(1)
 
 	log.Infof("Created counter for website %s", uri.Hostname())
 	return nil
 	// TODO
+}
+
+func (r *RouteHandler) handleRequestsCount(c echo.Context) error {
+	resp, err := http.Get("http://graphite/render?target=sumSeries(seriesByTag(%27hostname=myaweseomwebsitecom%27))&format=json")
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return c.String(http.StatusOK, string(b))
 }
 
 func (r *RouteHandler) handleEvent(c echo.Context) error {
@@ -58,4 +76,5 @@ func SetupRoutes(e *echo.Echo) {
 
 	e.POST("/view/", r.handleView)
 	e.POST("/event/", r.handleEvent)
+	e.GET("/requestcounts/", r.handleRequestsCount)
 }
--- benchmark/main.go
+++ benchmark/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -15,21 +16,52 @@ type ViewObj struct {
 func main() {
 	url := "http://localhost:8080/view/"
 
-	payload := ViewObj{
-		IP:        "127.0.0.1",
-		Useragent: "myuseragent",
-		URI:       "https://myaweseomwebsite.com/horseelevators",
-	}
-	json, err := json.Marshal(payload)
+	for i := 0; i < 1000; i++ {
+		log.Printf("Run %d", i)
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
-	req.Header.Set("Content-Type", "application/json")
+		payloads := []ViewObj{
+			{
+				IP:        "127.0.0.1",
+				Useragent: "myuseragent",
+				URI:       "https://myaweseomwebsite.com/horseelevators",
+			},
+			{
+				IP:        "127.255.0.1",
+				Useragent: "myuseragent5",
+				URI:       "https://myaweseomwebsite.com/horseelevators3",
+			},
+			{
+				IP:        "127.252.0.1",
+				Useragent: "myuseragent8",
+				URI:       "https://myaweseomwebsite.com/horseelevators2358923958",
+			},
+		}
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
+		for _, payload := range payloads {
+
+			json, err := json.Marshal(payload)
+
+			req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
+			req.Header.Set("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+		}
 	}
-	defer resp.Body.Close()
+
+	for i := 0; i < 100; i++ {
+		resp, err := http.Get("http://localhost:8080/requestcounts/")
+		if err != nil {
+			panic(err)
+		}
+
+		if resp.StatusCode != 200 {
+			panic(resp.StatusCode)
+		}
+	}
 
 }
